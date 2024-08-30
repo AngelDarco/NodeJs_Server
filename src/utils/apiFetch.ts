eb829c4bdc2aaf254d561de75be13e289924a06e
angeldarco <angel92-eme@hotmail.com>
2024-08-30 12:29:20 -0500

    :racehorse: fetch to call api created

---
+++ src/utils/apiFetch.ts
@@ -0,0 +1,8 @@
+const apiFetch = async () => {
+	const url = "http://localhost:3000/";
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
+
+export default apiFetch;
