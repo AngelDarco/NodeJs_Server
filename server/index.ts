db17414cebaf413d27c594c03dd9bb5ac4ff4a72
angeldarco <angel92-eme@hotmail.com>
2024-09-03 02:27:50 -0500

    :sparkles: server updated

--- server/index.ts
+++ server/index.ts
@@ -10,47 +10,49 @@ app.use(express.json());
 
 // allow cors
 app.use((_, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept",
-	);
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 // get all datadase
 app.get("/", (_, res) => {
-	res.send(db);
+  res.send(db);
 });
 
 // get data by id
 app.get("/miduapi/:id", (req, res) => {
-	const id = req.params.id;
-	const result = db.filter((item) => item.id.toString() === id);
-	res.send(result);
+  const id = req.params.id;
+  const result = db.filter((item) => item.id.toString() === id);
+  res.send(result);
 });
 
 // post data
 app.post("/miduapi", (req, res) => {
-	const { autor, frase } = req.body;
+  const { autor, frase, image } = req.body;
 
-	const schema = z.object({
-		autor: z.string(),
-		frase: z.string(),
-	});
-	const result = schema.safeParse(req.body);
-	if (result.success) {
-		db.push({ id: db.length + 1, autor, frase });
-		res.send(req.body).status(201);
-	} else {
-		res.send(result.error).status(400);
-	}
+  const schema = z.object({
+    autor: z.string(),
+    frase: z.string(),
+  });
+  const result = schema.safeParse(req.body);
+  if (result.success) {
+    db.push({ id: db.length + 1, autor, frase, image });
+    res.send(req.body).status(201);
+  } else {
+    res.send(result.error).status(400);
+  }
 });
 
 app.use((_, res) => {
-	res.send("<h1>405 Not Found</h1>").status(404);
+  res.send("<h1>405 Not Found</h1>").status(404);
 });
 
 app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
+
+//
