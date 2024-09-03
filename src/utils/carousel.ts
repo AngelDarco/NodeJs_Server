4a495623af0ee38bb8091fb132aaea8636484431
angeldarco <angel92-eme@hotmail.com>
2024-09-03 02:24:59 -0500

    :sparkles: carousel to show api phrases updated

--- src/utils/carousel.ts
+++ src/utils/carousel.ts
@@ -1,41 +1,48 @@
 import apiFetch from "../utils/apiFetch";
 
 document.addEventListener("astro:page-load", async () => {
-	interface ApiData {
-		id: number;
-		autor: string;
-		frase: string;
-		imagen?: string;
-	}
-	const data: ApiData[] = await apiFetch();
+  interface ApiData {
+    id: number;
+    autor: string;
+    frase: string;
+    image?: string;
+  }
+  const data: ApiData[] = await apiFetch();
 
-	const btnLeft = document.getElementById("btnLeft");
-	const btnRight = document.getElementById("btnRight");
+  const btnLeft = document.getElementById("btnLeft");
+  const btnRight = document.getElementById("btnRight");
 
-	const frase = document.getElementById("frase");
-	const autor = document.getElementById("autor");
-	const img = document.getElementById("img");
+  const frase = document.getElementById("frase");
+  const autor = document.getElementById("autor");
+  const img = document.getElementById("img");
 
-	let index = 0;
+  let index = 0;
 
-	if (frase) frase.innerHTML = data[index].frase;
-	if (autor) autor.innerHTML = data[index].autor;
-	if (img && data[index].imagen)
-		img.setAttribute("src", data[index].imagen || "");
+  // first load values
+  if (frase) frase.innerHTML = data[index].frase;
+  if (autor) autor.innerHTML = data[index].autor;
+  if (img && data[index].image)
+    img.setAttribute("src", data[index].image || "");
 
-	if (btnLeft)
-		btnLeft.addEventListener("click", () => {
-			index--;
-			if (index <= 0) index = data.length - 1;
-			if (frase) frase.innerHTML = data[index].frase;
-			if (autor) autor.innerHTML = data[index].autor;
-		});
+  // left arrow
+  if (btnLeft)
+    btnLeft.addEventListener("click", () => {
+      index--;
+      if (index < 0) index = data.length - 1;
+      if (frase) frase.innerHTML = data[index].frase;
+      if (autor) autor.innerHTML = data[index].autor;
+      if (img)
+        img.setAttribute("src", data[index].image || "/assets/midudev.jpg");
+    });
 
-	if (btnRight)
-		btnRight.addEventListener("click", () => {
-			index++;
-			if (index === data.length) index = 0;
-			if (frase) frase.innerHTML = data[index].frase;
-			if (autor) autor.innerHTML = data[index].autor;
-		});
+  // right arrow
+  if (btnRight)
+    btnRight.addEventListener("click", () => {
+      index++;
+      if (index === data.length) index = 0;
+      if (frase) frase.innerHTML = data[index].frase;
+      if (autor) autor.innerHTML = data[index].autor;
+      if (img)
+        img.setAttribute("src", data[index].image || "/assets/midudev.jpg");
+    });
 });
