d66ce8876859cecc350854cd9e9203ac78b7be96
angeldarco <angel92-eme@hotmail.com>
2024-09-12 14:04:06 -0500

    :hammer: new api linked

--- src/utils/apiFetch.ts
+++ src/utils/apiFetch.ts
@@ -1,5 +1,6 @@
 const apiFetch = async () => {
-  const url = "http://localhost:3000/";
+  // const url = "http://localhost:3000/";
+  const url = "https://angeldarco.github.io/server-nodejs/database/db.json";
   const response = await fetch(url);
   const data = await response.json();
   return data;
